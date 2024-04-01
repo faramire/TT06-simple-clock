272850ee6abe0a071f88419ce793394dbab771c4
faramire <github@faramire.de>
2024-04-02 01:27:00 +0200

    fixed counter enable chaining

--- src/counter_chain.v
+++ src/counter_chain.v
@@ -27,47 +27,48 @@ module counter_chain (
     counter10 inst_ces_0X ( // counts first digit centiseconds
         .clk (clk), // clock in
         .ena (ena), // enable
-        .res (res)  // reset
-        .max (ces_X0_ena) // reached max value, used as enable for the next counter
+        .res (res),  // reset
+        .max (ces_X0_ena), // reached max value, used as enable for the next counter
         .cnt (ces_0X) // output value
     );
 
     counter10 inst_ces_X0 ( // counts second digit centiseconds
         .clk (clk),
-        .ena (ces_X0_ena),
-        .res (res)
-        .max (sec_0X_ena)
+        .ena (ena & ces_X0_ena),
+        .res (res),
+        .max (sec_0X_ena),
         .cnt (ces_X0)
     );
 
     counter10 inst_sec_0X ( // counts first digit seconds
         .clk (clk),
-        .ena (sec_0X_ena),
-        .res (res)
-        .max (sec_X0_ena)
+        .ena (ena & ces_X0_ena & sec_0X_ena),
+        .res (res),
+        .max (sec_X0_ena),
         .cnt (sec_0X)
     );
 
     counter6 inst_sec_X0 ( // counts second digit seconds
         .clk (clk),
-        .ena (sec_X0_ena),
-        .res (res)
-        .max (min_0X_ena)
+        .ena (ena & ces_X0_ena & sec_0X_ena & sec_X0_ena),
+        .res (res),
+        .max (min_0X_ena),
         .cnt (sec_X0)
     );
 
     counter10 inst_min_0X ( // counts single digit minutes
         .clk (clk),
-        .ena (min_0X_ena),
-        .res (res)
-        .max (min_X0_ena)
+        .ena (ena & ces_X0_ena & sec_0X_ena & sec_X0_ena & min_0X_ena),
+        .res (res),
+        .max (min_X0_ena),
         .cnt (min_0X)
     );
 
     counter6 inst_min_X0 ( // counts second digit minutes
         .clk (clk),
-        .ena (min_X0_ena),
-        .res (res)
+        .ena (ena & ces_X0_ena & sec_0X_ena & sec_X0_ena & min_0X_ena & min_X0_ena),
+        .res (res),
+        .max (),
         .cnt (min_X0)
     );
 
